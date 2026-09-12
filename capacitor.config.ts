75c1d559770026f982aedf5d97e9dfefec816388
Dex <dex@vicecoast.local>
2026-09-12 13:15:45 +0000

    Fix cap sync (drop TS config) and lock app to landscape orientation

--- capacitor.config.ts
+++
@@ -1,9 +0,0 @@
-import type { CapacitorConfig } from '@capacitor/cli';
-
-const config: CapacitorConfig = {
-  appId: 'com.dex.vicecoast',
-  appName: 'Vice Coast',
-  webDir: 'www'
-};
-
-export default config;
